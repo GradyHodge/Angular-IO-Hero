a84eb23d94a361cfb67c96419ebf0d660a0061cf
GradyHodge <grady.hodge@DAL1404-m.nsi.int>
2019-10-15 15:12:02 -0500

    Angular Shell setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-IO-Tutorial';
-  subtitle = "My Angular Project based on IO documentation"
+  subtitle = 'My Angular Project based on IO documentation';
 }
